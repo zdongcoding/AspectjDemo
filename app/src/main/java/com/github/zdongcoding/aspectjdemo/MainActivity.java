18e69dce7319e42ebbdbaae2ab49ed274a2efedc
zoudong <zoudongq123@163.com>
2017-07-18 15:01:05 +0800

    Aspentj 语法简要说明

--- app/src/main/java/com/github/zdongcoding/aspectjdemo/MainActivity.java
+++ app/src/main/java/com/github/zdongcoding/aspectjdemo/MainActivity.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +25,29 @@ public class MainActivity extends BaseActivity {
             @Override
             public void onClick(View view) {
                 onDdd();
+                throwNullPoint();
                 startActivity(new Intent(MainActivity.this, SecondActivity.class));
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
     }
-    protected  void onDdd(){
-
+    private void throwNullPoint(){
+        String s=null;
+        s.equals("null");
     }
+    protected  void onDdd(){
+        test01();
+        Log.e("zoudong", "onDdd====" + "");
+    }
+    private  void   test(){
+        test01();
+        Log.e("zoudong", "test====" + "");
+    }
+    private  void test01(){
+        Log.e("zoudong", "test01====" + "");
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
--- aspect/src/main/java/com/github/zdongcoding/aspect/ActivityLifeAspect.java
+++ aspect/src/main/java/com/github/zdongcoding/aspect/ActivityLifeAspect.java
@@ -5,9 +5,13 @@ import android.util.Log;
 import android.widget.Toast;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 
 /**
@@ -18,31 +22,70 @@ public class ActivityLifeAspect {
 
     private static final String TAG = ActivityLifeAspect.class.getSimpleName();
 
-    @Before("execution (* com.github.zdongcoding.aspectjdemo.MainActivity.*(..))")
+    @Before("execution (* com.github.zdongcoding.aspectjdemo.MainActivity.onCreate(..))")
     public void adviceOnCreateBefore(JoinPoint joinPoint) {
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
         Toast.makeText((Context) joinPoint.getTarget(), "Before" + signature.toShortString(), Toast.LENGTH_SHORT).show();
         Log.e(TAG, "" + joinPoint.getSourceLocation() + "Before" + signature.toShortString());
     }
 
-    @After("execution (* com.github.zdongcoding.aspectjdemo.MainActivity.*(..))")
+    @After("execution (* com.github.zdongcoding.aspectjdemo.MainActivity.onCreate(..))")
     public void adviceOnCreateAfter(JoinPoint joinPoint) {
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
         Toast.makeText((Context) joinPoint.getTarget(), "After" + signature.toShortString(), Toast.LENGTH_SHORT).show();
         Log.e(TAG, "" + joinPoint.getSourceLocation() + "After" + signature.toShortString());
     }
-
+    @Around("execution (* com.github.zdongcoding.aspectjdemo.MainActivity.onDdd())")
+    public void adviceOnDDAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        Log.e("zoudong", "adviceOnDDAround====" + "joinPoint1 = [" + joinPoint.getSignature() + "]");
+        joinPoint.proceed();
+        Log.e("zoudong", "adviceOnDDAround====" + "joinPoint2 = [" + joinPoint.getSignature() + "]");
+    }
+    @Around("call (* com.github.zdongcoding.aspectjdemo.MainActivity.onDdd())")
+    public void adviceOnDDAroundCall(ProceedingJoinPoint joinPoint) throws Throwable {
+        Log.e("zoudong", "adviceOnDDAroundCall====" + "joinPoint1 = [" + joinPoint.getSignature() + "]");
+        joinPoint.proceed();
+        Log.e("zoudong", "adviceOnDDAroundCall====" + "joinPoint2 = [" + joinPoint.getSignature() + "]");
+    }
     /**
      * 只能修改当前类有的方法（子类方法必须重写）
      *v
      * @param joinPoint
      * @throws Throwable
      */
-    @Before("execution(* android.app.Activity.on**(..))")
-    public void onActivityMethodBefore(JoinPoint joinPoint) throws Throwable {
-        Log.e(TAG, "onActivityMethodBefore:------ start");
-        Log.e(TAG, joinPoint.getTarget().getClass().getSimpleName()+"-------------"+joinPoint.getSourceLocation().toString());
-        Log.e(TAG, "method   ->  " + joinPoint.getSignature().getName());
+//    @Before("execution(* android.app.Activity.on**(..))")
+//    public void onActivityMethodBefore(JoinPoint joinPoint) throws Throwable {
+//        Log.e(TAG, "onActivityMethodBefore:------ start");
+//        Log.e(TAG, joinPoint.getTarget().getClass().getSimpleName()+"-------------"+joinPoint.getSourceLocation().toString());
+//        Log.e(TAG, "method   ->  " + joinPoint.getSignature().getName());
+//
+//    }
 
+    @AfterThrowing(pointcut = "execution(* com.github.zdongcoding.aspectjdemo.MainActivity.throwNullPoint(..))",throwing="exception")
+    public void  throwNullPoint(Exception exception){
+        Log.e("zoudong", "throwNullPoint====" + " exception = [" + exception.toString() + "]");
+    }
+
+    public static final String invokeonDdd="withincode(* com.github.zdongcoding.aspectjdemo.MainActivity.onDdd(..))";
+    public static final String test01="call(* com.github.zdongcoding.aspectjdemo.MainActivity.test01(..))";
+    @Pointcut(invokeonDdd)
+    public void invokeOnDdd(){
+
+    }
+    @Pointcut(test01)
+    public void invoketest01(){
+
+    }
+    @Pointcut("invokeOnDdd()&&invoketest01()")
+    public void invoketest01OnlyOnDdd(){
+
+    }
+    @Before("invoketest01OnlyOnDdd()")
+    public void  invoketest01OnlyOnDddBefore(JoinPoint joinPoint){
+        Log.e("zoudong", "invoketest01OnlyOnDddBefore====" + "joinPoint = [" + joinPoint.getSourceLocation() + "]");
+    }
+    @After("invoketest01()&&invokeOnDdd()")
+    public void  invoketest01OnlyOnDddAfter(JoinPoint joinPoint){
+        Log.e("zoudong", "invoketest01OnlyOnDddAfter====" + "joinPoint = [" + joinPoint.getSourceLocation() + "]");
     }
 }
